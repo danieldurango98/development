372fcee26c974ad00921226d16b4e8c2efd7a2cb
Daniel Durango <danieldurango98@gmail.com>
2024-02-23 22:58:24 -0500

    working through test file

---
+++ class2/variables_operators.py
@@ -0,0 +1,119 @@
+#Print Built in Function
+
+# greeting = 'Happy Wednesday'
+# print(greeting)
+
+
+# first_name = 'Daniel'
+# print(first_name)
+
+# Addition
+
+# print(4 + 2)
+# print(5 + 5)
+# print(100 + 5)
+
+# Subtraction
+
+# print(10 - 3)
+# print(4 - 3)
+# print(10 - 5)    
+      
+# Multiplication
+
+# print(3 * 3)
+# print(2 * 8)
+# print(5 *7)
+
+# Division
+
+# print(10 / 2)
+# print(5 / 3)
+# print(5 / 0)
+
+# try:
+#     print(5 / 0)
+# except ZeroDivisionError as e:
+#     print("You cannot divide by zero, have a great day!")    
+
+# Exponents
+print(5 ** 5)
+print(2 ** 5)
+print(3 ** 6)
+
+# Integer Division
+
+
+# Modulo/Mod/Remainder
+
+
+# Program to find the perimeter of a rectangle
+
+
+# Data Types
+
+
+# Integer
+
+
+# String
+
+
+# Bool
+
+
+# Float
+
+
+# String
+
+
+# List 
+
+
+# Dictionary
+
+
+# Cast a string to an integer
+
+
+
+# Cast integer to string
+
+
+# Student grade dataset
+
+
+# finding amount of assignments
+
+
+# Perimeter of a rectangle
+
+
+# Determine perimeter and display output
+
+
+# Fahrenheit to Celsius
+
+
+# Eval
+
+
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+'''
+
+
+
+
+
+
+
+
+
