e91e976020713ab7c672bac65c7736a64e27c2d7
Daniel Durango <danieldurango98@gmail.com>
2024-03-20 20:37:20 -0400

    saving work again

--- class12/class_12_listspt2.py
+++ class12/class_12_listspt2.py
@@ -154,25 +154,32 @@ data = [[45,56,89],
 # print(data_columns)              
 
 # Using numpy
-data_columns = np.transpose(data)
+# data_columns = np.transpose(data)
 
 # Format for output
-print(f'''
-Column 1: Sum = , {sum(data_columns[0])}, Average = {statistics.mean(data_columns[0])}
-Column 2: Sum = , {sum(data_columns[1])}, Average = {statistics.mean(data_columns[1]) :.2f}      
-Column 3: Sum = , {sum(data_columns[2])}, Average = {statistics.mean(data_columns[2])}
-''')
+# print(f'''
+# Column 1: Sum = , {sum(data_columns[0])}, Average = {statistics.mean(data_columns[0])}
+# Column 2: Sum = , {sum(data_columns[1])}, Average = {statistics.mean(data_columns[1]) :.2f}      
+# Column 3: Sum = , {sum(data_columns[2])}, Average = {statistics.mean(data_columns[2])}
+# ''')
 
 # List Comprehension
 
 # For Loop
 # Add veggies less than 6 letters to new list
-vegetables = ['broccoli', 'kale', 'onion', 'garlic', 'kale']
+vegetables = ['broccoli', 'kale', 'onion', 'garlic']
 
+short_vegetables = []
+# for v in vegetables:
+#     if len(v) < 6: # if vegetable is less than 6 characters
+#         short_vegetables.append(v) # append to short vegetables
+# print(short_vegetables)
 
 # List comprehension
 vegetables = ['broccoli', 'kale', 'onion', 'garlic', 'kale']
 
+# new_list = [v for v in vegetables if len(v) < 6]
+# print(new_list)
 
 ''' Exercise
 You are given a list of integers. Write a Python program to create a new list that only includes the even numbers from the original list.
@@ -187,30 +194,39 @@ new_list = [34, 92, 2]
 original_list = [34, 57, 81, 92, 2, 13]
 new_list = []
 
+# for o in original_list:
+#     if o % 2 == 0:
+#         new_list.append(o)
 
 
 # With list comprehension
 original_list = [34, 57, 81, 92, 2, 13]
 
+# new_list = [o for o in original_list if o % 2 == 0]
+# print(new_list)
 
 
 ''' List comprehension with expressions'''
 
 # Lets add 5 to every item in this list
-numbers = [0, 2, 3, 8, 9, 11, 20]
-
+# numbers = [0, 2, 3, 8, 9, 11, 20]
+# plus_five = [n + 5 for n in numbers]
+# print(plus_five)
 
 # We can add two lists
 first_list = [1, 2, 3]
 second_list = [4, 5, 6]
 
+# third_list = [x for x in first_list] + [x for x in second_list]
+# print(third_list)
 
 
 # Multiply items from two lists
 list1 = [1, 2, 3, 4, 5]
 list2 = [10, 20, 30, 40, 50]
 
-
+# product_list = [list1[i] * list2[i] for i in range(len(list1))]
+# print(product_list)
 
 '''
 Exercise
@@ -224,11 +240,18 @@ Output: ['Dominic', 'Simone']
 
 '''
 
-over_60_years = ['Dominic', 'Linda', 'Simone', 'Swathi', 'Olaf']
-over_5_purchases = ['Finn', 'Simone', 'Aaron', 'Dominic']
+# over_60_years = ['Dominic', 'Linda', 'Simone', 'Swathi', 'Olaf']
+# over_5_purchases = ['Finn', 'Simone', 'Aaron', 'Dominic']
 
-customer_discount = []
+# customer_discount = []
 
-# For Loop
+# # For Loop
+# for c in over_60_years:
+#     if c in over_5_purchases:
+#         customer_discount.append(c)
+# # print(customer_discount)
 
-# List comprehension
+# # List comprehension
+
+# customer_discount = [c for c in over_60_years if c in over_5_purchases]
+# print(customer_discount)
